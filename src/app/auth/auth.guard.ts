268646ccc8345bd103a6d40182fa8a4426fe8c90
Lucas Zeni <zeni.lucas@gmail.com>
2025-05-12 20:10:00 -0300

    adicinando validacao para usuario admin

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -16,11 +16,27 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
-    const token = localStorage.getItem('token'); 
-    if (token) {
-      return true; 
+      const token = localStorage.getItem('token');
+      if (!token) {
+          this.router.navigate(['/login']);
+          return false;
+        }
+
+      try {
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        const role = payload.role;
+
+        if (role === 'admin') {
+          return true;
+        } else {
+          this.router.navigate(['/404']);
+          return false;
+        }
+      } catch (e) {
+        this.router.navigate(['/login']);
+        return false;
+      }
     }
-    this.router.navigate(['/login']); 
-    return false;
   }
 }
+
